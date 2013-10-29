696881ff962e563b1b51d99e6de27707bf9f0d64
sajeruk <sajeruk@gmail.com>
2013-10-29 21:11:44 +0300

    Added StumbleUpon.R - script, converting data to good format, modified README.md - Usage section appeared

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 My solution for kaggle.com competition StumbleUponEvergreen
 Uses naive bayes and boilerplate to predict results
+
+Usage:
+First run StumbleUpon.R (don't forget to change path to working dir in it)
+Then you'll have 2 files 'train.txt' and 'test.txt' in correct formats
+Then you can just run python file (just set output filename and last flag
+as False), if you want to perform some check - just write a short script, which
+separetes train sample in two randomly, so you can now check solution, just
+removing two last args in main() function
+Good luck ;) 
---
+++ StumbleUpon.R
@@ -0,0 +1,73 @@
+#read data; parseToWordList; clearBoilerPlate; writeToFileInFormat
+
+#read data
+workingDirectory <- 'D:\\Development\\Projects\\SDA\\O-course\\hometask1\\data\\test'
+train <- read.table(file.path(workingDirectory, "train.tsv"), sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+test <- read.table(file.path(workingDirectory, "test.tsv"), sep = "\t", header = TRUE, stringsAsFactors = FALSE)
+
+library(rjson)
+jsonToWordList <- function(json) {
+  #requires library rjson
+  boilerplate <- unlist(fromJSON(json))
+  names(boilerplate) <- NULL
+  boilerplate <- paste(boilerplate,collapse=" ")
+  boilerplate <- unlist(strsplit(boilerplate, " "))
+  return (boilerplate)
+}
+
+clearData = function(data) {
+  ret <- list()
+  for (text in data) {
+    u <- mapply(function(str) { if (nchar(str) <= 2 || str == "the" || nchar(str) > 25) return(FALSE) else return(TRUE)}, text)
+    add <- tolower(text[u])
+    ret <- append(ret, list(add))
+  }
+  return(ret)  
+}
+
+resultData <- list()
+resultTest <- list()
+
+#parseToWordList
+#this step will take a while
+for(i in 1:length(train$boiler)) {
+  resultData <- append(resultData, list(jsonToWordList(train$boiler[i])))
+}
+
+for(i in 1:length(test$boiler)) {
+  resultTest <- append(resultTest, list(jsonToWordList(test$boiler[i])))
+}
+
+#clearing data
+#this step also will take a while
+resultData <- clearData(resultData)
+resultTest <- clearData(resultTest)
+
+writeToFile <- function(filename, data, label = NULL, id = NULL) {
+  fileCon <- file(filename)
+  open(fileCon, open="w")
+  if (!is.null(label)) {
+    labels <- as.character(label)
+  }
+  if (!is.null(id)) {
+    ids <- as.character(id)
+  }
+  for (i in 1 : length(data)) {
+    if (!is.null(id)) {
+      writeLines(ids[i], fileCon, sep = ' ')
+    }
+    if (!is.null(label)) {
+      writeLines(labels[i], fileCon, sep = ' ')
+    }
+    writeLines(data[[i]], fileCon, sep = ' ')
+    writeLines("", fileCon)
+  }  
+  close(fileCon)
+}
+
+#finally writing files
+#format in each line: id label boilerplate
+writeToFile(file.path(workingDirectory, "train.txt"), resultData, train$label, train$urlid)
+#format in each line: id boilerplate
+writeToFile(file.path(workingDirectory, "test.txt"), resultTest, id = test$urlid)
+
